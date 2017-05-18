3c644a633579ee09c754dcee20e75c98abb1fd00
capJavert <ante.baric3@gmail.com>
2017-05-18 16:57:05 +0200

    added font awesome, added map items creation gui

--- src/app/maps/map.component.ts
+++ src/app/maps/map.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {ApplicationRef, Component} from '@angular/core';
 import { LatLngLiteral } from '@agm/core';
+declare let $:any;
+const ICON_SIZE = 40;
 
 @Component({
   selector: 'app-map',
@@ -7,66 +9,113 @@ import { LatLngLiteral } from '@agm/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-  // google maps zoom level
-  zoom: number = 8;
-  paths : Array<LatLngLiteral> = [
-    { lat: 51.373858,  lng: 7.815982 },
-    { lat: 51.673858,  lng: 7.215982 },
-    { lat: 51.373858,  lng: 7.895982 }
-    ];
-  // initial center position for the map
+  zoom: number = 9;
+  areas : Array<Area> = [];
   lat: number = 51.673858;
   lng: number = 7.815982;
+  isToolActive = false;
+  activeTool: string = null;
+  customTool: string = null;
+  customClassName: string = null;
+  mapStyle: any = [{"elementType":"geometry","stylers":[{"color":"#1d2c4d"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#8ec3b9"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#1a3646"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#64779e"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"color":"#334e87"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#023e58"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#283d6a"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#6f9ba5"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#023e58"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#3C7680"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#304a7d"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"road.arterial","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#2c6675"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#255763"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#b0d5ce"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#023e58"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#283d6a"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#3a4762"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#0e1626"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#4e6d70"}]}];
+  areaColor: string = "green";
+
+  constructor(private application: ApplicationRef) {}
 
   clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
+    if(this.customTool == "DELETE") {
+      this.markers.splice(index, 1);
+    }
   }
 
-  click(event: any){
-    console.log(`click event is called {event}`);
-  }
-
-  remove(event: any){
-    console.log(`delete is called {event}`);
+  clickedPaths(index: number){
+    if(this.customTool == "DELETE") {
+      this.areas.splice(index, 1);
+    }
   }
 
   mapClicked(event: any) {
-    this.markers.push({
-      lat: event.coords.lat,
-      lng: event.coords.lng,
-      draggable: true
-    });
+    if(this.isToolActive) {
+      switch (this.customTool) {
+        case "DELETE":
+          break;
+        case "AREA":
+          this.areas.push(
+            {
+              paths: [
+                {lat: event.coords.lat, lng: event.coords.lng},
+                {lat: event.coords.lat+0.1, lng: event.coords.lng+0.1},
+                {lat: event.coords.lat+0.1, lng: event.coords.lng-0.1}
+              ],
+              color: this.areaColor
+            }
+
+
+          );
+          break;
+        default:
+          let canvas = document.createElement("canvas");
+          canvas.width = ICON_SIZE;
+          canvas.height = ICON_SIZE;
+          //document.body.appendChild(canvas);
+          let ctx = canvas.getContext("2d");
+          ctx.fillStyle = "#ffffff";
+          ctx.font = ICON_SIZE + "px FontAwesome";
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillText(this.activeTool, ICON_SIZE/2, ICON_SIZE/2);
+
+          this.markers.push({
+            lat: event.coords.lat,
+            lng: event.coords.lng,
+            draggable: true,
+            iconUrl: canvas.toDataURL('image/png')
+          });
+      }
+    }
   }
 
-  markerDragEnd(m: marker, event: any) {
+  markerDragEnd(m: Marker, event: any) {
     console.log('dragEnd', m, event);
   }
 
-  markers: marker[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: true
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
+  markers: Marker[] = [];
+
+  onToolPick(unicode, className, custom = null): void {
+    this.customTool = custom;
+    this.activeTool = unicode;
+    this.isToolActive = true;
+    this.customClassName = className;
+  }
+
+  onColorPick(color): void {
+    this.areaColor = color;
+  }
+
+  onResetTool(): void {
+    if(this.isToolActive) {
+      this.activeTool = null;
+      this.isToolActive = false;
+      this.customTool = null;
+      this.customClassName = null;
+      this.application.tick();
     }
-  ];
+  }
+
+  toggleFAB(toggle): void {
+    toggle ? $('.fixed-action-btn').openFAB() : $('.fixed-action-btn').closeFAB();
+  }
 }
 
-interface marker {
+interface Marker {
   lat: number;
   lng: number;
   label?: string;
+  iconUrl?: string;
   draggable: boolean;
+}
+
+interface Area {
+  paths: Array<LatLngLiteral>;
+  color: string;
 }
